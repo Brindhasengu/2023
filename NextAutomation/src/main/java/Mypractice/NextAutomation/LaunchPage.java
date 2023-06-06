b4d760cfdae7583550b3db39bff3672acb4ba9b8
Brindhasengu <86748620+Brindhasengu@users.noreply.github.com>
2023-06-06 10:13:58 +0100

    Next page launch

--- NextAutomation/src/main/java/Mypractice/NextAutomation/LaunchPage.java
+++ NextAutomation/src/main/java/Mypractice/NextAutomation/LaunchPage.java
@@ -1,12 +1,13 @@
 package Mypractice.NextAutomation;
 import java.util.List;
 import java.util.Set;
-import org .openqa.selenium.By;
-import org .openqa.selenium.By.ById;
-import org .openqa.selenium.WebDriver;
-import org .openqa.selenium.WebDriver.Window;
-import org .openqa.selenium.WebElement;
-import org .openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.By;
+import org.openqa.selenium.By.ById;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebDriver.Window;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
 //import org .openqa.selenium.remote.server.handler.CloseWindow;
 
 public class LaunchPage {
@@ -23,8 +24,14 @@ driver.get("https://www.next.co.uk/");
 Thread.sleep(3000);
 WebElement AcceptAll=driver.findElement(By.id("onetrust-accept-btn-handler"));
 AcceptAll.click();
+WebElement searchbox = driver.findElement(By.id("header-big-screen-search-box"));
+searchbox.sendKeys("Mens Jenes");
+searchbox.sendKeys(Keys.ENTER);
+WebElement image= driver.findElement(By.xpath("//*[@id=\"platform_modernisation_product_summary_A13022\"]//div//div[1]//div[1]//div//div//div[1]//a//img"));
+image.click();
 
 //System.out.println(searchBox1);
+
 //System.out.println(searchBox2);
 	}
 
