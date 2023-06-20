ac9b351449837b9fbe73652ed6d155c607b07bb9
Brindhasengu <86748620+Brindhasengu@users.noreply.github.com>
2023-06-20 22:00:28 +0100

    Local Time

---
+++ Mywork/src/main/java/DateandTime.java
@@ -0,0 +1,34 @@
+import java.io.BufferedReader;
+import java.time.LocalDateTime;  
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class DateandTime {
+
+	public DateandTime() {
+		// TODO Auto-generated constructor stub
+	}
+
+				
+			public static void main (String[] args)
+					 throws IOException
+			{
+				System.out.println("What is your Name");
+				// Enter data using BufferReader
+		        BufferedReader reader = new BufferedReader(
+		            new InputStreamReader(System.in));
+				String name = reader.readLine();
+				System.out.println(" Hello "   +   name);
+				LocalDateTime now = LocalDateTime.now();  
+				if(now.getHour()<11)
+				{
+				System.out.println("good Morning ");
+				}
+				else
+				{
+					System.out.println("Good evening");
+				}
+				
+			}}
+
+
