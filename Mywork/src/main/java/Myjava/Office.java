f821967047aa8ec88402db5d87084d1353a2a2a5
Brindhasengu <86748620+Brindhasengu@users.noreply.github.com>
2023-06-21 22:21:59 +0100

    private

---
+++ Mywork/src/main/java/Myjava/Office.java
@@ -0,0 +1,22 @@
+package Myjava;
+
+public class Office {
+	private int profit=100;
+
+	//public Office() {
+		// TODO Auto-generated constructor stub
+		
+					
+			
+
+			public static void main(String[] args) {
+				// TODO Auto-generated method stub
+		//Office owner=new Office();
+		System.out.println("owner.profit");
+			}
+
+		}
+
+	
+
+	
