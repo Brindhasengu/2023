5285322766ba23168c66a3336ff2b386710efd02
Brindhasengu <86748620+Brindhasengu@users.noreply.github.com>
2023-06-22 21:10:45 +0100

    Java First pro

---
+++ Mywork/src/main/java/Myjava/FirstPro.java
@@ -0,0 +1,30 @@
+package Myjava;
+
+public class FirstPro {
+
+	public FirstPro() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+FirstPro pro=new FirstPro();
+pro.add();
+pro.subtract();
+	}
+public void add() 
+{
+	int no1=18,no2=20;
+	int total =no1+no2;
+	System.out.println(total);
+}
+
+public void subtract()
+{
+	int no1=50,no2=20;
+	int total=no1-no2;
+System.out.println(total);
+
+}
+
+}
