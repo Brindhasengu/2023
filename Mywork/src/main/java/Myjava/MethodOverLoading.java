51e070c08c40f3dccd673677aed6db6be777f3f9
Brindhasengu <86748620+Brindhasengu@users.noreply.github.com>
2023-06-26 17:08:32 +0100

    Method Over Loading1

---
+++ Mywork/src/main/java/Myjava/MethodOverLoading.java
@@ -0,0 +1,22 @@
+package Myjava;
+
+public class MethodOverLoading 
+{
+public int MethodOveLoading(int X,int y) {return(X+y);}
+public int MethodOverLoading(int X,int Y,int Z)
+{
+	return(X+Y+Z);
+}
+	public MethodOverLoading()
+	{
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args)
+	{
+		// TODO Auto-generated method stub
+MethodOverLoading mol= new MethodOverLoading();
+System.out.println(mol.MethodOverLoading(10, 20, 30));
+	}
+
+}
