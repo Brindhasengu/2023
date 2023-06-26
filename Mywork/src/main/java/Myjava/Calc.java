b0d6c23dcd4d5f9cf64c196b9c2403ca2433440a
Brindhasengu <86748620+Brindhasengu@users.noreply.github.com>
2023-06-26 16:25:39 +0100

    Method overloading

---
+++ Mywork/src/main/java/Myjava/Calc.java
@@ -0,0 +1,36 @@
+package Myjava;
+
+public class Calc {
+
+	public Calc() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args)
+{
+		// TODO Auto-generated method stub
+Calc cc = new Calc();
+int result =cc.add();
+cc.subtract(result);
+cc.subtract(50,5);
+cc.subtract(50,6.6f);
+	}
+public void subtract(int num1,float num2)
+{
+System.out.println(num1-num2);	
+}
+public void subtract(int num)
+{
+	
+}
+public void subtrat(int value,int value2)
+{
+	
+}
+public int add()
+{
+	int no1=10,no2=20;
+	return no1+no2;
+	
+}
+}
