833030762259d6f984d6b08bfc53d3cce9f2bea2
Brindhasengu <86748620+Brindhasengu@users.noreply.github.com>
2023-06-28 16:20:57 +0100

    private constructor

---
+++ Mywork/src/main/java/Myjava/PconsSchool.java
@@ -0,0 +1,24 @@
+package Myjava;
+
+public class PconsSchool 
+{
+
+	public PconsSchool()
+	{
+		// TODO Auto-generated constructor stub
+	System.out.println("I,m private constructor");	
+	}
+private int profit=100;
+private String name="Abc";
+	public static void main(String[] args)
+	{
+		// TODO Auto-generated method stub
+		PconsSchool owner=new PconsSchool();
+		System.out.println(owner.profit);
+		owner.promote();
+	}
+private void promote()
+{
+	System.out.println("Students list");
+}
+}
