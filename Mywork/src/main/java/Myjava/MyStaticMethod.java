3c2277523cddf7410f771cbd16f3347f3d4c73c1
Brindhasengu <86748620+Brindhasengu@users.noreply.github.com>
2023-06-29 22:37:57 +0100

    Static method

---
+++ Mywork/src/main/java/Myjava/MyStaticMethod.java
@@ -0,0 +1,19 @@
+package Myjava;
+
+public class MyStaticMethod {
+static void mystaticmethod()
+{
+	System.out.println("Static method can called without main");
+}
+
+	public MyStaticMethod() {
+		// TODO Auto-generated constructor stub
+	}
+public static void main(String[]args) 
+{
+mystaticmethod();
+{
+	
+}
+}
+}
