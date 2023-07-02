8058b252bd5c1bb39c12f0868248d46b0b7c5e2a
Brindhasengu <86748620+Brindhasengu@users.noreply.github.com>
2023-07-02 21:37:58 +0100

    Constructor over loading

---
+++ Mywork/src/main/java/Myjava/ConsOverLoading.java
@@ -0,0 +1,21 @@
+package Myjava;
+
+public class ConsOverLoading {
+
+	public ConsOverLoading() 
+	{
+		// TODO Auto-generated constructor stub
+		System.out.println("Constructor one");
+	}
+public  ConsOverLoading(String S)
+{
+	System.out.println("Constructor 2");
+	
+}
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		ConsOverLoading a = new ConsOverLoading();
+		ConsOverLoading b =new ConsOverLoading("");
+				
+				
+	}}
