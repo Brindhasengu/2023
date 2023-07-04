067597053184b31b504dd1e6e4cca245ca80596b
Brindhasengu <86748620+Brindhasengu@users.noreply.github.com>
2023-07-04 22:47:38 +0100

    Swap

---
+++ Mywork/src/main/java/Myjava/Swap.java
@@ -0,0 +1,21 @@
+package Myjava;
+import java.util.*;  
+public class Swap {
+
+	public Swap() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		 int x, y, t;// x and y are to swap   
+	       Scanner sc = new Scanner(System.in);  
+	       System.out.println("Enter the value of X and Y");  
+	       x = sc.nextInt();  
+	       y = sc.nextInt();  
+	       System.out.println("before swapping numbers: "+x +"  "+ y);  
+	
+
+	}
+
+}
