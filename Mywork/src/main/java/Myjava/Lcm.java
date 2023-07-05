286365d284dce49dd66db488c74c55e4d5ce4a78
Brindhasengu <86748620+Brindhasengu@users.noreply.github.com>
2023-07-05 10:24:00 +0100

    LCM

---
+++ Mywork/src/main/java/Myjava/Lcm.java
@@ -0,0 +1,22 @@
+package Myjava;
+
+public class Lcm {
+
+	public Lcm() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+int no1= 3;
+int no2= 5;
+int result= no1*no2;
+
+if (result%no1==0   && result%no2==0)
+    {
+	System.out.println(result);
+	}
+
+}
+}
