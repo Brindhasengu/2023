2d0d8237de72a62fa36bb86d7e4536c7d3a6b1a2
Brindhasengu <86748620+Brindhasengu@users.noreply.github.com>
2023-07-07 16:41:16 +0100

    For

---
+++ Mywork/src/main/java/Myjava/ForLoop.java
@@ -0,0 +1,19 @@
+package Myjava;
+
+public class ForLoop {
+
+	public ForLoop() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+//int num;
+for (int num=1 ;num<=9; num++)
+{
+	System.out.println(num);
+}
+	}
+
+}
+  
