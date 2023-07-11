39432d08b5b01b0752ea1a3911f3bfaa42bc45b9
Brindhasengu <86748620+Brindhasengu@users.noreply.github.com>
2023-07-11 13:52:22 +0100

    For patterns

---
+++ Mywork/src/main/java/myjava/Forprac.java
@@ -0,0 +1,34 @@
+package myjava;
+
+public class Forprac {
+
+	public Forprac() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int x=5;
+		for(int row=1;row<=5;row++)
+		{
+		
+	//
+        for(int col=1;col<row; col++)
+{
+	System.out.print(" ");
+}
+{
+	//
+}
+for(int col=1;col<=x; col++)
+{
+	System.out.print(x+" ");
+	
+}
+System.out.println();
+x--;
+	}
+		}
+	}
+
+
