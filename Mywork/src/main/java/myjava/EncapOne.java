67bc91503083c10cb00612291c85f96d1e037fec
Brindhasengu <86748620+Brindhasengu@users.noreply.github.com>
2023-07-14 16:03:40 +0100

    Encapsulation

---
+++ Mywork/src/main/java/myjava/EncapOne.java
@@ -0,0 +1,19 @@
+package myjava;
+
+public class EncapOne {
+	
+	private String name;
+	
+	//Getter
+	public String getName()
+	{
+		return name;
+	}
+	//Settter
+	public void setName(String newName)
+	{
+		this.name=newName;
+		
+	}
+	
+}
---
+++ Mywork/src/main/java/myjava/MainEncapone.java
@@ -0,0 +1,16 @@
+package myjava;
+
+public class MainEncapone {
+
+	public MainEncapone() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+EncapOne myObj= new EncapOne();
+myObj.setName("Ammu");
+System.out.println(myObj.getName());
+	}
+
+}
