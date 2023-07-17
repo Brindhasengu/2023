1555efa140344cdc1a2099a7a45b1518f5e1e9b0
Brindhasengu <86748620+Brindhasengu@users.noreply.github.com>
2023-07-17 14:58:58 +0100

    Interface

---
+++ Mywork/src/main/java/myjava/IFaceClass.java
@@ -0,0 +1,18 @@
+package myjava;
+
+public class IFaceClass implements In1 {
+
+	// Implementing the capabilities of
+    // interface.
+    public void display(){
+      System.out.println("Implements Interface");
+    }
+ 
+    // Driver Code
+    public static void main(String[] args)
+    {
+        IFaceClass t = new IFaceClass();
+        t.display();
+        System.out.println(a);
+    }
+}
---
+++ Mywork/src/main/java/myjava/In1.java
@@ -0,0 +1,11 @@
+package myjava;
+import java.io.*;
+public interface In1 
+{
+	final int a = 20;
+	 
+    // public and abstract
+    void display();
+}
+
+
