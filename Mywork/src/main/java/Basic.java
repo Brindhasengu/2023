088e863b9abd08c95f2124d00f26370ec8be47b2
Brindhasengu <86748620+Brindhasengu@users.noreply.github.com>
2023-07-20 13:40:19 +0100

    First Basic pro

--- Mywork/src/main/java/Basic.java
+++ Mywork/src/main/java/Basic.java
@@ -1,17 +1,20 @@
 
-public class Basic {
-int x;
+public class Basic 
+{
+int x;//create attribute
 int y;
-	public Basic() {
+	public Basic() 
+	{
 		// TODO Auto-generated constructor stub
-		x=5;
+		x=5;//set  value for attribute
 		y=10;
 	}
 
-	public static void main(String[] args) {
+	public static void main(String[] args) 
+	{
 		// TODO Auto-generated method stub
-Basic obj= new Basic();
-System.out.println(obj.x);
+Basic obj= new Basic();//create obj
+System.out.println(obj.x);//print x value
 System.out.println(obj.y);
 	}
 
---
+++ Mywork/src/main/java/myjava/JavaFirst.java
@@ -0,0 +1,14 @@
+package myjava;
+
+public class JavaFirst {
+
+	public JavaFirst() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("This is java basic programme");
+	}
+
+}
