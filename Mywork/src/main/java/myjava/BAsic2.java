3298796b0a8be80fc4845cff94cd096488680296
Brindhasengu <86748620+Brindhasengu@users.noreply.github.com>
2023-07-21 13:26:48 +0100

    Basic string

---
+++ Mywork/src/main/java/myjava/BAsic2.java
@@ -0,0 +1,16 @@
+package myjava;
+
+public class BAsic2 {
+
+	public BAsic2() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args)
+	{
+		// TODO Auto-generated method stub
+System.out.println(10+20+"String check");
+System.out.println("String check"+10+20);
+	}
+
+}
