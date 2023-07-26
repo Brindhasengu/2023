dac04948004697c4841df4233664927d1900962f
Brindhasengu <86748620+Brindhasengu@users.noreply.github.com>
2023-07-26 11:38:45 +0100

    Basic If else

---
+++ Mywork/src/main/java/myjava/Basic6.java
@@ -0,0 +1,24 @@
+package myjava;
+
+public class Basic6 {
+
+	public Basic6() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) 
+	{
+		//defining a variable  
+	    int number=13;  
+	    //Check if the number is divisible by 2 or not  
+	    if(number%2==0){  
+	        System.out.println("even number");  
+	    }else{  
+	        System.out.println("odd number");  
+	    }  
+	}  
+	
+
+	}
+
+
