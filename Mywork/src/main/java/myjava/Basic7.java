4499abdb88b1ba3db948701f0923725f61d2619d
Brindhasengu <86748620+Brindhasengu@users.noreply.github.com>
2023-07-27 12:12:17 +0100

    Basic

---
+++ Mywork/src/main/java/myjava/Basic7.java
@@ -0,0 +1,20 @@
+package myjava;
+
+public class Basic7 {
+int x=10;
+	public Basic7() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+
+			  
+			   Basic7 myObj = new Basic7();
+			    System.out.println(myObj.x);
+	}
+			}
+	
+
+
