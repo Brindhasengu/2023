a9c2174c8b17f1e686d2d6b4467d99ed8d0e7630
Brindhasengu <86748620+Brindhasengu@users.noreply.github.com>
2023-07-28 22:17:38 +0100

    DataT

---
+++ Mywork/src/main/java/myjava/Basic8.java
@@ -0,0 +1,27 @@
+package myjava;
+
+public class Basic8 {
+
+	public Basic8() {
+		// TODO Auto-generated constructor stub
+	}
+
+	 public static void main(String[] args)
+     {
+         // byte type
+         byte b = 5;
+         System.out.println("b= " + b);
+
+         // short type
+         short s = 20;
+         System.out.println("s= " + s);
+
+         // int type
+         int i = 200;
+         System.out.println("i= " + i);
+
+         // long type
+         long l = 123456789;
+         System.out.println("l= " + l);
+     }
+ }
