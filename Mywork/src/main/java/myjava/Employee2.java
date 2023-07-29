170f7fa89dbbf61034f33ec638078779287c7086
Brindhasengu <86748620+Brindhasengu@users.noreply.github.com>
2023-07-29 17:22:53 +0100

    Emp2

---
+++ Mywork/src/main/java/myjava/Employee2.java
@@ -0,0 +1,35 @@
+package myjava;
+import java.io.*;
+public class Employee2 {
+
+	
+	
+
+	   // this instance variable is visible for any child class.
+	   public String name;
+
+	   // salary  variable is visible in Employee class only.
+	   private double salary;
+
+	   // The name variable is assigned in the constructor.
+	   public Employee2 (String empName) {
+	      name = empName;
+	   }
+
+	   // The salary variable is assigned a value.
+	   public void setSalary(double empSal) {
+	      salary = empSal;
+	   }
+
+	   // This method prints the employee details.
+	   public void printEmp() {
+	      System.out.println("name  : " + name );
+	      System.out.println("salary :" + salary);
+	   }
+
+	   public static void main(String args[]) {
+	      Employee2 empOne = new Employee2("Anbu");
+	      empOne.setSalary(1000);
+	      empOne.printEmp();
+	   }
+	}
