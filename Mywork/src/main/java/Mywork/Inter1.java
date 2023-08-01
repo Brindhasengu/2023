6bc3f3d20978c94efd889ed7aa60fd3cc3c29af2
Brindhasengu <86748620+Brindhasengu@users.noreply.github.com>
2023-08-01 21:33:58 +0100

    Implements

---
+++ Mywork/src/main/java/Mywork/Inter1.java
@@ -0,0 +1,15 @@
+package Mywork;
+import java.io.*;
+public interface Inter1 {
+	
+	 
+	// A simple interface
+	
+	   
+	    // public, static and final
+	    final int a = 10;
+	 
+	    // public and abstract
+	    void display();
+
+}
---
+++ Mywork/src/main/java/Mywork/InterTestClass.java
@@ -0,0 +1,18 @@
+package Mywork;
+
+import java.io.*;
+public class InterTestClass implements Inter1 {
+
+	public JavaFirst() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("This is java basic programme");
+	}
+
+}
+
+
+
