e0c684cd00aecc4ce64ef3e9c38e4e0c1cd2d014
Brindhasengu <86748620+Brindhasengu@users.noreply.github.com>
2023-08-05 17:12:17 +0100

    If else

---
+++ IdeaProjects/Project1/src/main/java/org/example/IfElseClass.java
@@ -0,0 +1,16 @@
+package org.example;
+
+public class IfElseClass {
+
+static int number=9;
+    public static void main(String[] args) {
+       // int number=9;
+        if (number%2==0){
+            System.out.println(number + "Tis number is even");
+        }
+        else{
+            System.out.println(number + "This is odd number");
+        }
+    }
+
+}
