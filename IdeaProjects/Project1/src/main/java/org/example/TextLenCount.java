7989e8ae3ee10fcad9163111024e21f794ae22bc
Brindhasengu <86748620+Brindhasengu@users.noreply.github.com>
2023-08-09 16:43:14 +0100

    If else

---
+++ IdeaProjects/Project1/src/main/java/org/example/TextLenCount.java
@@ -0,0 +1,11 @@
+package org.example;
+
+public class TextLenCount
+{
+    public static void main(String[] args) {
+            String txt = "Senguttuvan";
+            System.out.println("The length of the txt string is: " + txt.length());
+        }
+    }
+
+
