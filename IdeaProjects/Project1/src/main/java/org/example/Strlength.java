77d99f62e21803ec6884d7fe3edc7006df2049ef
Brindhasengu <86748620+Brindhasengu@users.noreply.github.com>
2023-08-12 15:13:52 +0100

    Switch case

---
+++ IdeaProjects/Project1/src/main/java/org/example/Strlength.java
@@ -0,0 +1,11 @@
+package org.example;
+
+public class Strlength
+{
+    public static void main(String[] args) {
+     String s1="SriKrishna" ;
+     String s2="SreeMatha";
+        System.out.println("string length:" +s1.length());
+        System.out.println("string lenth:" +s2.length());
+    }
+}
