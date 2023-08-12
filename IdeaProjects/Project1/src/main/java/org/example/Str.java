5bbb8dc3d0b0f24c1790e0fca7c48a3837f07ff3
Brindhasengu <86748620+Brindhasengu@users.noreply.github.com>
2023-08-12 14:43:32 +0100

    Switch case

---
+++ IdeaProjects/Project1/src/main/java/org/example/Str.java
@@ -0,0 +1,14 @@
+package org.example;
+
+public class Str {
+    public static void main(String[] args) {
+        String s1="java";// literal obj
+        char ch[]={'s','t','r','i','n','g','s'};
+        String s2=new String(ch);//converting char array to string//String obj
+        String s3=new String("example");//creating Java string by new keyword
+        System.out.println(s1);
+        System.out.println(s2);
+        System.out.println(s3);
+
+    }
+}
---
+++ IdeaProjects/Project1/src/main/java/org/example/WhileLoop.java
@@ -0,0 +1,17 @@
+package org.example;
+
+public class WhileLoop {
+    public static void main(String[] args) {
+        //print1 to 10
+        int i = 1;//int i= 0 //This will print
+        while (i <= 10) {
+            System.out.println(i);
+            i += 2;//so we can print odd number
+        }
+
+
+
+
+    }
+
+}
