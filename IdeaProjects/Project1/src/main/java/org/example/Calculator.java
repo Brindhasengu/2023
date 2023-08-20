3eb5b48a957ee70908a2e3c2249587a69c5333be
Brindhasengu <86748620+Brindhasengu@users.noreply.github.com>
2023-08-20 18:44:23 +0100

    MyArray

--- IdeaProjects/Project1/src/main/java/org/example/Calculator.java
+++ IdeaProjects/Project1/src/main/java/org/example/Calculator.java
@@ -4,11 +4,19 @@ public class Calculator {
     public static void main(String[] args) {
         Calculator calc = new Calculator();
         calc.add();
+        var result = calc.multi();
+        System.out.println(result);
     }
         public void add () {
             int a = 33, b = 75;
             int total = a + b;
             System.out.println(total);
+        }
+        public int multi(){
+        int x=10,  y=20;
+        int total = x*y ;
+            //System.out.println(total);
+            return total;
         }
     }
 
