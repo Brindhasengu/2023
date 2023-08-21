d4a2ad2325aba01922d413a5a78532fd5b1b8f43
Brindhasengu <86748620+Brindhasengu@users.noreply.github.com>
2023-08-21 14:57:12 +0100

    Business

---
+++ IdeaProjects/Project1/src/main/java/org/example/Business.java
@@ -0,0 +1,10 @@
+package org.example;
+
+public class Business {
+    private int profit=1000;
+    public static void main(String[] args) {
+        Business owner= new Business();
+        System.out.println(owner.profit);
+
+    }
+}
---
+++ IdeaProjects/Project1/src/main/java/org/example/MyPm.java
@@ -0,0 +1,15 @@
+package org.example;
+/*
+
+public class MyPm {
+    public static void main(String[] args) {
+        int a=10,b=20;
+        for(int i=0;i<10;i++) {
+            static int m = 0 ;
+            m = m + 1;
+
+        }
+
+    }
+}
+*/
