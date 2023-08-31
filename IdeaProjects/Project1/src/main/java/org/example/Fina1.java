7baa7574207f442e605d7d5604e14c8d3c4c619a
Brindhasengu <86748620+Brindhasengu@users.noreply.github.com>
2023-08-31 13:38:06 +0100

    Fina1.java

---
+++ IdeaProjects/Project1/src/main/java/org/example/Fina1.java
@@ -0,0 +1,16 @@
+package org.example;
+
+public class  Fina1 {
+    final int balance =100;
+    public static void main(String[] args) {
+        Fina1 obj1 =new Fina1();
+        obj1.work();
+
+    }
+public final void work(){
+    {
+        System.out.println("Fina 1 work");
+    }
+}
+
+}
---
+++ IdeaProjects/Project1/src/main/java/org/example/Fina2.java
@@ -0,0 +1,14 @@
+package org.example;
+
+public class Fina2 extends Fina1{
+    public static void main(String[] args) {
+        Fina2 obj = new Fina2();
+      obj.work();
+       // System.out.println(obj.balance);
+    }
+    public void work(){
+        {
+            System.out.println("Fina2 work");
+        }
+    }
+}
