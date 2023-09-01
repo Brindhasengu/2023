e048dfa764045edf5a165ffecdc8208bf1018f37
Brindhasengu <86748620+Brindhasengu@users.noreply.github.com>
2023-09-01 14:06:08 +0100

    Abstra.java

--- IdeaProjects/Project1/src/main/java/org/example/Fina1.java
+++ IdeaProjects/Project1/src/main/java/org/example/Fina1.java
@@ -3,14 +3,19 @@ package org.example;
 public class  Fina1 {
     final int balance =100;
     public static void main(String[] args) {
+
+    }
+    /*
         Fina1 obj1 =new Fina1();
         obj1.work();
 
     }
-public final void work(){
+public void work(){
     {
         System.out.println("Fina 1 work");
     }
+
+ */
 }
 
-}
+
