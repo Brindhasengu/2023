0c7af090743451d2941487853dd0db3deb506d9a
Brindhasengu <86748620+Brindhasengu@users.noreply.github.com>
2023-09-01 14:05:35 +0100

    Abstra.java

---
+++ IdeaProjects/Project1/src/main/java/org/example/Abstra.java
@@ -0,0 +1,15 @@
+package org.example;
+
+public abstract class Abstra {
+    public static void main(String[] args) {
+
+    }
+ public abstract void work();
+    public void holiday(){
+        System.out.println("Number of holiday");
+    }
+    public void promotion()
+    {
+        System.out.println("Promotion");
+    }
+}
