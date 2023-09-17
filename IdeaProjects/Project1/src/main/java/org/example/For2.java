d71995c679cd3f53f2319421cb398002335c1415
Brindhasengu <86748620+Brindhasengu@users.noreply.github.com>
2023-09-17 21:12:55 +0100

    For2

---
+++ IdeaProjects/Project1/src/main/java/org/example/For2.java
@@ -0,0 +1,12 @@
+package org.example;
+
+public class For2 {
+    public static void main(String[] args) {
+        for (int i= 5; i>=1; i--){
+            for(int no =1;no<=5; no++){
+                System.out.print(i);
+            }
+            System.out.println();
+        }
+    }
+}
---
+++ IdeaProjects/Project1/src/main/java/org/example/Whileme.java
@@ -0,0 +1,11 @@
+package org.example;
+
+public class Whileme {
+    public static void main(String[] args) {
+        int no=1;
+        while(no<=9) {
+            System.out.println(no);
+            no++;
+        }
+        }
+    }
