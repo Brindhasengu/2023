81e8e1c5599f67b15500a8adcf3f3f1f0f34fbe3
Brindhasengu <86748620+Brindhasengu@users.noreply.github.com>
2023-10-30 21:54:14 +0000

    Update Dropdown.java

--- IdeaProjects/Salinaka/src/test/java/AutomationPractice/Dropdown.java
+++ IdeaProjects/Salinaka/src/test/java/AutomationPractice/Dropdown.java
@@ -9,14 +9,28 @@ public class Dropdown {
     public static void main(String[] args) {
         WebDriver driver=new ChromeDriver();
 
-        driver.get("https://www.moneysavingexpert.com/");
+        driver.get("https://www.bbc.co.uk/");
         driver.manage().window().maximize();
 
-        WebElement Acceptall = driver.findElement(By.id("banner-accept"));
-        Acceptall.click();
+        WebElement youracc = driver.findElement(By.className("ssrcss-qgttmg-AccountText e1gviwgp4"));
+        youracc.click();
 
 
-        WebElement dd= driver.findElement(By.className("nav-tip-popup"));
-    dd.click();
+        WebElement email = driver.findElement(By.id("user-identifier-input"));
+        WebElement next = driver.findElement(By.id("submit-button"));
+        next.click();
+
+//Get the Web Element corresponding to the Password Field
+        WebElement password = driver.findElement(By.id("user_password"));
+
+//Find the Sign me in button
+        WebElement login = driver.findElement(By.id("user_submit"));
+
+        email.sendKeys("brindhasengu@gmail.com");
+        password.sendKeys("Anga1234!");
+
+        login.click();
+        System.out.println("Signed in with Click");
+        driver.close();
     }
 }
