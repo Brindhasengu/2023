323942b33709dd16384d456bb412fd588f1242bf
Brindhasengu <86748620+Brindhasengu@users.noreply.github.com>
2023-11-03 16:49:32 +0000

    BHomepage

---
+++ IdeaProjects/Bahmni/src/test/java/BahmniHomepage.java
@@ -0,0 +1,13 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class BahmniHomepage
+{
+    public static void main(String[] args) {
+        WebDriver driver=new ChromeDriver();
+        driver.get("https://demo-lite.mybahmni.in/");
+        driver.manage().window().maximize();
+    }
+}
