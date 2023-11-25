90f2c4524ca211ac123d05ff112159c56f1e8eaf
Brindhasengu <86748620+Brindhasengu@users.noreply.github.com>
2023-11-25 22:03:21 +0000

    Count Str

---
+++ IdeaProjects/Salinaka/src/test/java/AutomationPractice/CountString.java
@@ -0,0 +1,13 @@
+package AutomationPractice;
+
+public class CountString {
+    public static void main(String[] args) {
+
+        String str = "This is a sample string";
+
+        int len = str.length();
+
+        System.out.println("Length of the String: " + len);
+
+    }
+}
--- IdeaProjects/Salinaka/src/test/java/AutomationPractice/Multiplication.java
+++ IdeaProjects/Salinaka/src/test/java/AutomationPractice/Multiplication.java
@@ -3,7 +3,7 @@ package AutomationPractice;
 public class Multiplication {
     public static void main(String[] args) {
 
-        int N=9;
+        int N=10;
         for(int i=1;i<=10;i++){
 
             System.out.println(N + " * " + i + " = "
