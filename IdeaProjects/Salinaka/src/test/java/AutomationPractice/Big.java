55b8daeb3449a7e75455d4f9118a60ebdcc10154
Brindhasengu <86748620+Brindhasengu@users.noreply.github.com>
2023-11-27 16:49:46 +0000

    Create Big.java

---
+++ IdeaProjects/Salinaka/src/test/java/AutomationPractice/Big.java
@@ -0,0 +1,11 @@
+package AutomationPractice;
+
+public class Big {
+    public static void main(String[] args) {
+        int x=10;
+        int y=20;
+        int large=x>y?x:y;
+        System.out.println("The large number is "+large);
+
+    }
+}
