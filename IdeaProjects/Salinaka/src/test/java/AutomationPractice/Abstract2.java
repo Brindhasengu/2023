b85e8f08d481ed4d748b3e1845632a446d80e245
Brindhasengu <86748620+Brindhasengu@users.noreply.github.com>
2023-12-03 19:29:30 +0000

    Abstract

---
+++ IdeaProjects/Salinaka/src/test/java/AutomationPractice/Abstract2.java
@@ -0,0 +1,9 @@
+package AutomationPractice;
+
+public class Abstract2 extends Abstract1 {
+    public void animalSound() {
+        // The body of animalSound() is provided here
+        System.out.println("The pig says: wee wee");
+    }
+
+}
---
+++ IdeaProjects/Salinaka/src/test/java/AutomationPractice/AbstractMain.java
@@ -0,0 +1,9 @@
+package AutomationPractice;
+
+public class AbstractMain {
+    public static void main(String[] args) {
+       Abstract2 myPig = new Abstract2(); // Create a Pig object
+        myPig.animalSound();
+        myPig.sleep();
+    }
+}
