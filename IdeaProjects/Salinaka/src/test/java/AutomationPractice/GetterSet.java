2e3bc0faea254782dca438d401ecdd51443ebf5a
Brindhasengu <86748620+Brindhasengu@users.noreply.github.com>
2023-12-05 11:04:52 +0000

    Getter Setter

---
+++ IdeaProjects/Salinaka/src/test/java/AutomationPractice/GetterSet.java
@@ -0,0 +1,8 @@
+package AutomationPractice;
+
+public class GetterSet {
+    private  int minBalance  =1000;
+    public  int getMinBalance(){
+        return this.minBalance;
+    }
+}
---
+++ IdeaProjects/Salinaka/src/test/java/AutomationPractice/Setterget.java
@@ -0,0 +1,10 @@
+package AutomationPractice;
+
+public class Setterget {
+    public static void main(String[] args) {
+        GetterSet vv= new GetterSet();
+       int min= vv.getMinBalance();
+        
+        System.out.println(min);
+    }
+}
