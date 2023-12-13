1f259923a58fe4fa5ecc8c7f4ae988e09180b038
Brindhasengu <86748620+Brindhasengu@users.noreply.github.com>
2023-12-13 22:09:39 +0000

    Constructor

---
+++ IdeaProjects/Salinaka/src/test/java/AutomationPractice/MyConstructor.java
@@ -0,0 +1,13 @@
+package AutomationPractice;
+
+public class MyConstructor {
+
+        //creating a default constructor
+        MyConstructor(){System.out.println("Constructor is created");}
+        //main method
+        public static void main(String args[]){
+//calling a default constructor
+           MyConstructor b=new MyConstructor();
+        }
+    }
+
