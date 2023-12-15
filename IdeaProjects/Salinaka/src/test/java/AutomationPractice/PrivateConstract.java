a09a2dc164c4882de83964cc30b80e6fcbfd338c
Brindhasengu <86748620+Brindhasengu@users.noreply.github.com>
2023-12-15 11:37:19 +0000

    Create PrivateConstract.java

---
+++ IdeaProjects/Salinaka/src/test/java/AutomationPractice/PrivateConstract.java
@@ -0,0 +1,14 @@
+package AutomationPractice;
+
+public class PrivateConstract {
+  private   PrivateConstract(){
+
+    }
+    void disply(){
+        System.out.println("Private contractor");
+    }
+
+    public static void main(String[] args) {
+        PrivateConstract obj=new PrivateConstract();
+    }
+}
