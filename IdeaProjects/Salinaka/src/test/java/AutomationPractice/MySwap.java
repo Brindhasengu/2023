4e61ab45d496b31c9932f94762351b7177face59
Brindhasengu <86748620+Brindhasengu@users.noreply.github.com>
2023-12-17 20:35:25 +0000

    Create MySwap.java

---
+++ IdeaProjects/Salinaka/src/test/java/AutomationPractice/MySwap.java
@@ -0,0 +1,19 @@
+package AutomationPractice;
+
+public class MySwap {
+    public static void main(String[] args) {
+        int x = 10;
+        int y = 20;
+        System.out.println("Before Swap");
+        System.out.println("x ="+ x);
+        System.out.println("y =" + y);
+        //Swap
+        int temp = x;
+        x = y;
+      y = temp;
+        System.out.println("After swap");
+        System.out.println("x ="+x );
+        System.out.println("y ="+y);
+
+    }
+}
