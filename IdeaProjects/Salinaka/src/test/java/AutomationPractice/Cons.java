f0cfbe5cc076576bbf15d3be2d47674bbe9d401d
Brindhasengu <86748620+Brindhasengu@users.noreply.github.com>
2023-12-20 21:58:03 +0000

    Create Cons.java

---
+++ IdeaProjects/Salinaka/src/test/java/AutomationPractice/Cons.java
@@ -0,0 +1,13 @@
+package AutomationPractice;
+
+public class Cons {
+
+        //creating a default constructor
+        Cons(){System.out.println(" Cons is created");}
+        //main method
+        public static void main(String args[]){
+//calling a default constructor
+            Cons b=new  Cons();
+        }
+    }
+
