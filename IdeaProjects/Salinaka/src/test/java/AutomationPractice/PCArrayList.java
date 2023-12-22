8fd66ba5dd4ca32cdeeb715bbcb69a81ce5144a3
Brindhasengu <86748620+Brindhasengu@users.noreply.github.com>
2023-12-22 20:21:31 +0000

    Create PCArrayList.java

---
+++ IdeaProjects/Salinaka/src/test/java/AutomationPractice/PCArrayList.java
@@ -0,0 +1,17 @@
+
+package AutomationPractice;
+import java.util.ArrayList;
+public class PCArrayList {
+    public static void main(String[] args) {
+       ArrayList al=new ArrayList();
+       // System.out.println(al.size());
+        al.add("Ammu");
+
+        al.add(10);
+        al.add(20);
+        System.out.println(al);
+        System.out.println(al.contains("Ammu"));
+    }
+
+
+}
