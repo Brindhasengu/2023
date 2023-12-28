b514942878b48fe4e623ce526e88239faaba54f4
Brindhasengu <86748620+Brindhasengu@users.noreply.github.com>
2023-12-28 12:29:45 +0000

    Create StaticM.java

---
+++ IdeaProjects/Salinaka/src/test/java/AutomationPractice/StaticM.java
@@ -0,0 +1,12 @@
+package AutomationPractice;
+
+public class StaticM {
+    static void Stmethod() {
+        System.out.println("Static method");
+    }
+
+    public static void main(String[] args) {
+         Stmethod();
+         Stmethod();
+    }
+}
