d6e838197ada374545bee39dfecb5d2b035ae552
Brindhasengu <86748620+Brindhasengu@users.noreply.github.com>
2023-12-29 21:35:04 +0000

    Reverse String

---
+++ IdeaProjects/Salinaka/src/test/java/Interview/StrCompare.java
@@ -0,0 +1,18 @@
+package Interview;
+
+//Compare 2 string whether equal or not
+public class StrCompare {
+
+
+        public static void main(String args[]){
+            String s1="Sachin";
+            String s2="Sachin";
+            String s3=new String("Sachin");
+            String s4="Dhoni";
+            System.out.println(s1.equals(s2));//true
+            System.out.println(s1.equals(s3));//true
+            System.out.println(s1.equals(s4));//false
+        }
+    }
+
+
