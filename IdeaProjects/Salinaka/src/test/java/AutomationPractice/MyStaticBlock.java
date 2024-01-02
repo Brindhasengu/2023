9ebde991577d71a898ff426a5b8317aa0607d8cd
Brindhasengu <86748620+Brindhasengu@users.noreply.github.com>
2024-01-02 21:35:36 +0000

    Create MyStaticBlock.java

---
+++ IdeaProjects/Salinaka/src/test/java/AutomationPractice/MyStaticBlock.java
@@ -0,0 +1,16 @@
+package AutomationPractice;
+
+public class MyStaticBlock {
+
+
+    // Static block
+    static {
+        // Print statement
+        System.out.print(
+                "Static block");
+    }
+
+    public static void main(String[] args) {
+    }
+}
+
