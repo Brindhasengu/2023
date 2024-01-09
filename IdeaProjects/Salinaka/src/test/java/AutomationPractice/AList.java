7a2137d4db9d7be2dd688d102bf9104bdd846d21
Brindhasengu <86748620+Brindhasengu@users.noreply.github.com>
2024-01-09 16:32:03 +0000

    Create AList.java

---
+++ IdeaProjects/Salinaka/src/test/java/AutomationPractice/AList.java
@@ -0,0 +1,16 @@
+package AutomationPractice;
+import java.util.ArrayList;
+public class AList {
+
+
+
+        public static void main(String[] args) {
+            ArrayList<String> cars = new ArrayList<String>();
+            cars.add("Volvo");
+            cars.add("BMW");
+            cars.add("Ford");
+            cars.add("Mazda");
+            System.out.println(cars);
+        }
+    }
+
