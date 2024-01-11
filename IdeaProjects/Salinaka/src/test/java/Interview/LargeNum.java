786ceb8b0f4386bbc5d1ea12f8c98601edfdf749
Brindhasengu <86748620+Brindhasengu@users.noreply.github.com>
2024-01-11 22:04:01 +0000

    Create LargeNum.java

---
+++ IdeaProjects/Salinaka/src/test/java/Interview/LargeNum.java
@@ -0,0 +1,14 @@
+package Interview;
+
+public class LargeNum {
+
+        public static void main(String[] args) {
+            int x=10;
+            int y=20;
+            int large=x>y?x:y;
+            System.out.println("The large number is "+large);
+
+        }
+    }
+
+
