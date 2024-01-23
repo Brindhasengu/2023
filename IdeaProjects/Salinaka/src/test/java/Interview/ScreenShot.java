51a3647a160822e8a944126e6e1bb47823fd15e0
Brindhasengu <86748620+Brindhasengu@users.noreply.github.com>
2024-01-23 21:56:48 +0000

    SS

---
+++ IdeaProjects/Salinaka/src/test/java/Interview/ScreenShot.java
@@ -0,0 +1,20 @@
+package Interview;
+import org.apache.commons.io.FileUtils;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.io.File;
+import java.io.IOException;
+
+public class ScreenShot {
+    public static void main(String[] args) throws IOException {
+
+       WebDriver driver= new ChromeDriver();
+       driver.get("https://www.thg.com/");
+        File scrFile = ((TakesScreenshot)driver).getScreenshotAs(OutputType.FILE);
+        // Now you can do whatever you need to do with it, for example copy somewhere
+        FileUtils.copyFile(scrFile, new File("c:\\temp\\screenshot.png"));
+    }
+}
